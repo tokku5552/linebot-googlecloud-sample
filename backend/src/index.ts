a2bb4055fb92722069b945d3774373e0c76546ae
徳田 真之介 <tokuda_shinnosuke@cyberagent.co.jp>
2024-02-27 23:59:55 +0900

    add: express pj

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
