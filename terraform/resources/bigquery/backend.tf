3150624fe3a7d8da7c0d0939070b9ff69f8d6f5e
oyasumipants <ibaraki_keigo@cyberagent.co.jp>
2024-03-14 00:56:49 +0900

    feat: bq

--- terraform/resources/bigquery/backend.tf
+++ terraform/resources/bigquery/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "linedc-handson-terraform-state" // your gcs bucket name
-    prefix = "bigquery"                        // your gcs prefix
+    prefix = "bigquery"                       // your gcs prefix
   }
 }
